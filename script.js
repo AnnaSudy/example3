942ff0d50c5e65c1a1f6ebc9db66777406e31814
Anna Sudy <anna.sudy.poczta@gmail.com>
2023-05-24 14:51:12 +0200

    JS functions added

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-
-let buttonElement = document.querySelector(".js-header__button");
-let bodyElement = document.querySelector(".js-body");
-let themeElement = document.querySelector(".js-header__themeName");
-
-buttonElement.addEventListener("click", () => {
+{
+  const changeBackground = () => {
+    const bodyElement = document.querySelector(".js-body");
+    const themeElement = document.querySelector(".js-header__themeName");
     bodyElement.classList.toggle("js-body--dark");
-    themeElement.innerText = bodyElement.classList.contains("js-body--dark") ? "jasny" : "ciemny";
-});
+    themeElement.innerText = bodyElement.classList.contains("js-body--dark")
+      ? "jasny"
+      : "ciemny"; 
+  };
+  
+  const init = () => {
+    const buttonElement = document.querySelector(".js-header__button");
+    buttonElement.addEventListener("click", changeBackground);
+  };
+  
+  init();
+}
