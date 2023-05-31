3f6a6ba61ae678cfbf71f1216d7fdc9c8828ff44
Anna Sudy <anna.sudy.poczta@gmail.com>
2023-05-31 14:54:52 +0200

    little changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 {
-  const changeBackground = () => {
+  const toggleBackground = () => {
     const bodyElement = document.querySelector(".js-body");
     const themeElement = document.querySelector(".js-header__themeName");
     bodyElement.classList.toggle("js-body--dark");
     themeElement.innerText = bodyElement.classList.contains("js-body--dark")
       ? "jasny"
-      : "ciemny"; 
+      : "ciemny";
   };
-  
+
   const init = () => {
     const buttonElement = document.querySelector(".js-header__button");
-    buttonElement.addEventListener("click", changeBackground);
+    buttonElement.addEventListener("click", toggleBackground);
   };
-  
+
   init();
 }
